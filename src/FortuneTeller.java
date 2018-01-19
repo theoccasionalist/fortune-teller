fa2d33860e24b8ec62df8c1d726d97cb45c23e9c
theoccasionalist <mrmccall07@gmail.com>
2018-01-19 13:51:52 -0500

    Parts 1 through 3 first attempt

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -6,7 +6,7 @@ public class FortuneTeller {
 		// Part 1: ask the user for intimate information about themselves.
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Hi!\nWelcom to part one of the interigation.\nWhat is your first name?");
+		System.out.println("Hi!\nWelcome to the interigation.\nWhat is your first name?");
 		String firstName = input.nextLine();
 
 		System.out.println("What is your last name?");
@@ -20,7 +20,7 @@ public class FortuneTeller {
 
 		System.out.println("How many siblings do you have?");
 		int sib = input.nextInt();
-		input.next();
+		input.nextLine();
 
 		System.out.println(
 				"Finally, what is your favorite ROYGBIV color?\nEnter \'help\' for the list of ROYGBIV colors.");
@@ -28,57 +28,63 @@ public class FortuneTeller {
 		while (color.contains("h")) { // attempting to account for user mistype; wouldn't work if a ROYGBIv contained
 										// an 'h'.
 			System.out.println(
-					"The list of ROYGBIV colors is as follows:\nRed\nOrange\nYellow\nGreen\nBlue\nIndigo\nViolet\n\nSee, ROYGBIV!\nPlease choose one of the above options.");
+					"The list of ROYGBIV colors is as follows:\n Red Orange Yellow Green Blue Indigo Violet\n\nSee, ROYGBIV!\nPlease choose one of the above options.");
 			color = input.nextLine();
 		}
 		input.close();
 
 		// Part 2: Calculating a Fortune
-
+	
+	int retirement;
 	 if(age%2==0) //calculating retirement age
 	{
-		int retirement = 12;
+		retirement = 12;
 	}else
 	{
-		int retirement = 14;
+		retirement = 14;
 	}
 	 
+	 String vacation;
 	 if (sib < 0) {//calculating vacation home location 
-		 String vacation = "Gary, IN"; 
+		 vacation = "Gary IN"; 
 	 }else if (sib == 0) {
-		 String vacation = "Pascagoula, MS";
+		 vacation = "Pascagoula MS";
 	 }else if (sib == 1) {
-		 String vacation = "Slidell, LA";
+		 vacation = "Slidell LA";
 	 }else if (sib == 2) {
-		 String vacation = "Pecos, TX";		 
+		 vacation = "Pecos TX";		 
 	 }else {
-		 String vacation = "Montgomery, AL";
+		 vacation = "Montgomery AL";
 	 }
 	 
+	 String transport;
 	 if (color.trim().equalsIgnoreCase("red")) {
-		 String transport = "Hybrid Hippo-Duck";
+		 transport = "hybrid Hippo-Duck";
 	 }else if (color.trim().equalsIgnoreCase("orange")) {
-		 String transport =  "Nappy, dirty dog";
+		 transport =  "nappy, dirty dog";
 	 }else if (color.trim().equalsIgnoreCase("yellow")) {
-		 String transport = "Steven's high-tech water bottle";
+		 transport = "steven's high-tech water bottle";
 	 }else if (color.trim().equalsIgnoreCase("green")) {
-		 String transport = "That one friend's bootleg car...you know, that ONE friend's car";
+		 transport = "that one friend's bootleg car...you know, that ONE friend's car";
 	 }else if(color.trim().equalsIgnoreCase("blue")) {
-		 String transport = "A house pig";
+		 transport = "a house pig";
 	 }else if(color.trim().equalsIgnoreCase("indigo")) {
-		 String transport = "The mean average of your childhood and adulthood.";
-	 }else if(color.trim().equalsIgnoreCase("violet")) {
-		 String transport = "That monkey from Indiana Jones and the Temple of Doom.";
+		 transport = "the physical manifestation of your childhood.";
+	 }else {
+		 transport = "that monkey from Indiana Jones and the Temple of Doom.";
 	 }
 	 
+	 double account;
 	 if (0 < month && month <5) {
-		 double account = 2.33;
+		 account = 2.33;
 	 }else if(4 < month && month < 9) {
-		 double account = 2.39;
+		 account = 2.39;
 	 }else if(9 < month && month < 13) {
-		 double account = 2.41;
+		 account = 2.41;
 	 }else {
-		 double account = 0.0;
+		 account = 0.0;
 	 }
+	 //Part 3: Fortune Projecting
+	 System.out.println(firstName + " " + lastName + " will retire in " + retirement + " years with $" + account + " in the bank, a vacation home in " + vacation + ", and travel by " + transport + ".");
 	}
 }
