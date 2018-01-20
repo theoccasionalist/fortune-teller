29faeed6c730caa71034322845aef7ca9b859bb8
theoccasionalist <mrmccall07@gmail.com>
2018-01-20 16:55:32 -0500

    Version 1.5

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -11,22 +11,24 @@ public class FortuneTeller {
 				"Hi!\nWelcome to Dismal Fortunes Incorporated.\nDFI's patented projection algorithms will help you cross the infinite bridge to happiness one step at a time.\nWe'll need some information to get started.\n(Enter \'quit\' at any time.)\nNow, let's begin.");
 		System.out.println();
 
-		System.out.println("What is your first name?"); // first name input
+		// first name input
+		System.out.println("What is your first name?");
 		String firstName = input.nextLine();
 		if (firstName.trim().equalsIgnoreCase("quit")) {
 			System.exit(0);
 		}
-		while (firstName.trim().equals("") ) {
+		while (firstName.trim().equals("")) { // not allowing empty strings as names
 			System.out.println("Sorry, empty strings aren't accepted as names.  We apologize to anyone named:");
 			firstName = input.nextLine();
 		}
 
-		System.out.println("What is your last name?"); // last name input
+		// last name input
+		System.out.println("What is your last name?");
 		String lastName = input.nextLine();
 		if (lastName.trim().equalsIgnoreCase("quit")) {
 			System.exit(0);
 		}
-		while (lastName.trim().equals("") ) {
+		while (lastName.trim().equals("")) {
 			System.out.println("Sorry, empty strings aren't accepted as names.  We apologize to anyone named:");
 			firstName = input.nextLine();
 		}
@@ -36,38 +38,34 @@ public class FortuneTeller {
 				"Note: the next set of questions must be answered with 0 or positive integer numerals (e.g. \'4\', \'45\', \'68\').");
 		System.out.println();
 
-		System.out.println("How many years you've been alive?"); // age input
+		// Age input
+		System.out.println("How many years have you been alive?"); // age input
 		String age = input.nextLine();
 		if (age.trim().equalsIgnoreCase("quit")) {
 			System.exit(0);
 		}
-		while (!((age.contains("0") || age.contains("1") || age.contains("2")) || age.contains("3") || age.contains("4")
-				|| age.contains("5") || age.contains("6") || age.contains("7") || age.contains("8")
-				|| age.contains("9"))) {
-			System.out.println("Please insert an integer numeral.  For example, \'15\', \'35\', \'47\'.");
-			age = input.nextLine();
-		}
-		while (Integer.parseInt(age) < 0) {
+		while (!(age.contains("0") || age.contains("1") || age.contains("2") || age.contains("3")
+				|| age.contains("4") || age.contains("5") || age.contains("6") || age.contains("7") || age.contains("8")
+				|| age.contains("9")) || Integer.parseInt(age) < 0) {
 			System.out.println("Please insert a positive integer numeral.  For example, \'15\', \'35\', \'47\'.");
 			age = input.nextLine();
 		}
 
-		System.out.println("What month were you born in?(Note: only values 1 - 12 will be accepted.)");// birth month input
+		// month born input
+		System.out.println("What month were you born in? (Note: only values 1 - 12 will be accepted.)");// birth month
+																										// input
 		String month = input.nextLine();
 		if (month.trim().equalsIgnoreCase("quit")) {
 			System.exit(0);
 		}
-		while (Integer.parseInt(month) <= 0 || Integer.parseInt(month) >= 13) {
-			System.out.println("Please input including or between 1 - 12.");
-			month = input.nextLine();
-		}
-		while (!((month.contains("0") || month.contains("1") || month.contains("2")) || month.contains("3")
-				|| month.contains("4") || month.contains("5") || month.contains("6") || month.contains("7")
-				|| month.contains("8") || month.contains("9"))) {
-			System.out.println("Please insert an integer numeral.  For example, \'15\', \'35\', \'47\'.");
+		while (!(month.contains("0") || month.contains("1") || month.contains("2") || month.contains("3")
+						|| month.contains("4") || month.contains("5") || month.contains("6") || month.contains("7")
+						|| month.contains("8") || month.contains("9")) || Integer.parseInt(month) <= 0 || Integer.parseInt(month) >= 13) {
+			System.out.println("Please input a positive integer between and including 1 - 12.");
 			month = input.nextLine();
 		}
 
+		// siblings input
 		System.out.println("How many siblings do you have?");// siblings input
 		String sib = input.nextLine();
 		if (sib.trim().equalsIgnoreCase("quit")) {
@@ -75,15 +73,12 @@ public class FortuneTeller {
 		}
 		while (!((sib.contains("0") || sib.contains("1") || sib.contains("2")) || sib.contains("3") || sib.contains("4")
 				|| sib.contains("5") || sib.contains("6") || sib.contains("7") || sib.contains("8")
-				|| sib.contains("9"))) {
-			System.out.println("Please insert an integer numeral.  For example, \'15\', \'35\', \'47\'.");
+				|| sib.contains("9")) || Integer.parseInt(sib) < 0 ) {
+			System.out.println("Please insert 0 or a positive numeral integer.  For example, \'15\', \'35\', \'47\'.");
 			sib = input.nextLine();
 		}
-		while (Integer.parseInt(sib) < 0) {
-			System.out.println("Please insert a positive integer numeral.  For example, \'15\', \'35\', \'47\'.");
-			age = input.nextLine();
-		}
 
+		// ROYGBIV input
 		System.out.println(
 				"Finally, what is your favorite ROYGBIV color?\n\n(Enter \'help\' for the list of ROYGBIV colors.)");// ROYGBIV
 																														// input
@@ -139,7 +134,7 @@ public class FortuneTeller {
 		} else if (color.trim().equalsIgnoreCase("green")) {
 			transport = "the clammy realization of your true motivations";
 		} else {
-			transport = "the bleakness of an uncolored life.";//catches non-ROYGBIV values for the variable 'color'
+			transport = "the bleakness of an unaccomplished life";// catches non-ROYGBIV values for the variable 'color'
 		}
 
 		double account; // calculating account balance
@@ -155,7 +150,7 @@ public class FortuneTeller {
 
 		// Part 3: Fortune Projection Output
 		System.out.println();
-		System.out.println("DFI projects that your fortune will be as follows: ");
+		System.out.println("DFI projects that your future will be as follows: ");
 		System.out.println();
 		System.out.println(firstName + " " + lastName + " will retire in " + retirement + " years with $" + account
 				+ " in the bank, a vacation home in " + vacation + ", and live with " + transport + ".");
